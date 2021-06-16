e03a19cd25d48c0b82b0efb80fcfe95a9e774379
NikitaSolynin <n.solynin@x-way.ru>
2021-06-16 17:00:40 +0300

    methods for defining the next note

--- NoteClass.py
+++ NoteClass.py
@@ -1,3 +1,5 @@
+import triangular_list
+
 class Note:
     def __init__(self, content:str, tags:set) -> None:
         self.content = content
@@ -8,16 +10,22 @@ class Book:
         self.len = 0
         self.notes = []
         self.tag_set = set()
-    
+        self.weights_table = triangular_list.Triangle(self.len)
+        self.make_new_weights_table = True
+
     def __add__(self, note:Note) -> None:
         self.len += 1
         self.notes.append(note)
         self.tag_set.update(note.tags)
+        self.weights_table.make_new_line()
+        self.make_new_weights_table = True
         return self
     
     def remove(self, ind:int) -> None:
         self.len -= 1
         self.notes.remove(ind)
+        self.weights_table.remove_line()
+        self.make_new_weights_table = True
 
     def __getitem__(self, ind:int) -> None:
         return self.notes[ind]
@@ -25,6 +33,33 @@ class Book:
     def __len__(self) -> int:
         return self.len
 
+    def IoU(A:set, B:set):
+        V = len(A.intersection(B))
+        U = len(A.update(B))
+        return V/U
+
+    def make_weights_table_between_notes(self):
+        for i in range(self.l-1):
+            for j in range(i+1, self.l):
+                self.weights_table[i, j] = self.IoU(self.__getitem__(i), self.__getitem__(j))
+
+    def next_note(self, ind:int):
+        """
+        ind - start index
+        """
+        if self.make_new_weights_table:
+            self.make_weights_table_between_notes()
+            self.make_new_weights_table = False
+        max_weight = False
+        res_ind = 0
+        for i in range(self.l):
+            if i == ind:
+                continue
+            if not max_weight or max_weight < self.weights_table[ind, i]:
+                max_weight = self.weights_table[ind, i]
+                res_ind = i
+        self.weights_table[ind, res_ind] -= 1
+        return res_ind
 class Library:
     def __init__(self) -> None:
         self.books = {}
